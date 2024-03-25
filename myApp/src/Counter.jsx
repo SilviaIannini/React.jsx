4d700419c7e2a541d0968dd76bcf2dd9b17ec165
SilviaIannini <ianninisilvia@gmail.com>
2024-03-25 22:07:32 +0100

    Counter.jsx

--- myApp/src/Counter.jsx
+++ myApp/src/Counter.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import { DisplayCounter } from "./DisplayCounter";
 export function Counter(){
-    const[counter,setCounter]=useState(0)
-    function handleClick(){
-        setCounter(counter + 1)
+ 
+    const [counter, setCounter]=useState(0)
+    
+    function HandleAu(){
+        setCounter(counter +1)
     }
+    function HandleDec(){
+        setCounter(counter -1)
+    }
+    function HandleReset(){
+        setCounter(0)
+    }
+    return(
+       <div>
+        <DisplayCounter count={counter}/>
+         <button onClick={HandleAu}>Aumenta</button>
+            <button onClick={HandleDec}>diminuisci</button>
+            <button onClick={HandleReset}>reset</button>
+       </div>
+          
+           
+       
+    )
 }
-return(
-    
-        <div>
-            <h1>ciao</h1>
-            <button onClick={handleClick}>conta</button>
-        </div>
-    
-)
