a6fc281ce3d19b122828877efafc641a9d9066aa
SilviaIannini <153084625+SilviaIannini@users.noreply.github.com>
2024-04-03 14:57:25 +0200

    Add files via upload

---
+++ Colors.jsx
@@ -0,0 +1,12 @@
+function Color({color}){
+return <li>{color.name}</li>
+}
+
+export function Colors({colors}){
+return(
+    <ul>
+        {colors.map((color)=>
+            <Color key={color.id} color={color}/>)}
+        
+    </ul>
+)}
---
+++ DisplayCounter.jsx
@@ -0,0 +1,14 @@
+
+export function DisplayCounter({count}){
+
+    return(
+    
+     <h1>conta fino a {count}
+     </h1>
+    
+        
+       
+       
+    
+  )
+}
